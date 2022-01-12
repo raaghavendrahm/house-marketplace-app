586df22481ef0f67eba1bd8140c200f1fa4b4168
raaghavnedrahm <raaghavendrahm@gmail.com>
2022-01-12 10:16:42 +0530

    Pages are created and Routing is done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+
 function App() {
   return (
     <>
-      <h1>House Marketplace</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          {/* SignIn will be replaced by Profile page later in the above route */}
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        {/* Navbar comes here next */}
+      </Router>
     </>
   );
 }
