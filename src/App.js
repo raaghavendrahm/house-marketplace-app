5f65c8c7218269df0d7c4466df45d12e292a8fc8
raaghavnedrahm <raaghavendrahm@gmail.com>
2022-01-14 08:15:54 +0530

    Fetching single listing on clicking a listing is started. Listing is obtained as an object in Listing page.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './components/PrivateRoute';
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
 
 function App() {
   return (
@@ -32,6 +33,10 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
         </Routes>
 
         <Navbar />
