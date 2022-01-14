f78605ba2408b131d11e1b0f96cbf0545dbc94ee
raaghavnedrahm <raaghavendrahm@gmail.com>
2022-01-14 10:55:03 +0530

    Map is embedded is Listing page using Leaflet, and react-leaflet packages.

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -6,6 +6,9 @@ import { db } from '../firebase.config';
 import Spinner from '../components/Spinner';
 import shareIcon from '../assets/svg/shareIcon.svg';
 
+// To embed maps using leaflet and react-leaflet
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 const Listing = () => {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -100,7 +103,26 @@ const Listing = () => {
         </ul>
 
         <p className="listingLocationTitle">Location</p>
-        {/* MAP comes here later */}
+
+        <div className="leafletContainer">
+          <MapContainer
+            style={{ height: '100%', width: '100%' }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+            />
+
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {/* If the person in the page is not the user who is logged in (authorized), then a button to contact the landlord should appear: */}
         {auth.currentUser?.uid !== listing.userRef && (
