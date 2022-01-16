f0f3afe87ff4ebb2d2725b4cf4e1629a3d7f33c7
raaghavnedrahm <raaghavendrahm@gmail.com>
2022-01-16 18:11:18 +0530

    Shifted from Emulator to Cloud.

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,7 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import { connectAuthEmulator, getAuth } from 'firebase/auth';
-import { getStorage, connectStorageEmulator } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,13 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
-
 export const db = getFirestore();
-export const auth = getAuth();
-const storage = getStorage();
-
-if (process.env.REACT_ENV !== 'production') {
-  connectFirestoreEmulator(db, 'localhost', 8080);
-  connectAuthEmulator(auth, 'http://localhost:9099');
-  connectStorageEmulator(storage, 'localhost', 9199);
-}
